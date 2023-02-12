7f67e27adfc10b43dcc7d6a1991df6fb6b120522
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-12 19:54:31 +0000

    added new function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,3 +37,13 @@ class FileStorage:
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
+            
