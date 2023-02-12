e33b4844361b9f45b1c3080e2f7a1dd27a25d83a
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-12 17:24:05 +0000

    added __str__ dunder magic methods

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,38 @@ class BaseModel:
    """
    def __init__(self, *args, **kwargs):
        """
+       Initializes the public instance attributes
        :param args:
        :param kwargs:
-       Inititializes public instance attributes
        """
        if (len(kwargs) ==0):
            self.id = str(uuid.uuid4())
            self.created_at = datetime.now()
            self.updated_at = datetime.now()
+       else:
+           kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+           kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+
+           for k, v in kwargs.items():
+               if "__class__" not in k:
+                   setattr(self, k, v)
+
+        def __str__(self):
+            """
+            Returns string representation of BaseModel class
+            :param self:
+            :return:
+            """
+            return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                          self.id, self.__dict__))
+
+        def save(self):
+            """
+            Update the updated_at attribute
+            :param self:
+            :return:
+            """
+
+
