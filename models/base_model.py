4155cdfd0652fda81c185cbae5f302402b2c209c
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-12 19:36:18 +0000

    to_dict added

--- models/base_model.py
+++ models/base_model.py
@@ -37,3 +37,30 @@ class BaseModel:
                 self.updated_at = self.created_at
 
 
+    def __str__(self):
+        """String representation  of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+
+
+    def save(self):
+        """Updates the attribute"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all key/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
