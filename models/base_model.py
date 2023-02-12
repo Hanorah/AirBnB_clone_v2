4779440072e3af7fabbb1c7596e66ca75e29762f
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-12 17:14:34 +0000

    added base class

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 """
-    
+   A BaseModel class that contains all attributes/methods for other classes
 """
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+   """
+      Base class for all other classes to use
+   """
+   def __init__(self, *args, **kwargs):
+       """
+       :param args:
+       :param kwargs:
+       Inititializes public instance attributes
+       """
+       if (len(kwargs) ==0):
+           self.id = str(uuid.uuid4())
+           self.created_at = datetime.now()
+           self.updated_at = datetime.now()
