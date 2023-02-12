56fb72b56fde74974dc0b6bba41868baf829747a
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-12 19:49:59 +0000

    filestorage class added

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Contains the filestorage class
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+classes = {
+    "BaseModel": BaseModel
+}
+
+class FileStorage:
+    """Serializes instances to a JSON file & deserializes back to instances"""
+
+    # string - path to the JSON file
+    __file_path = "file.json"
+
+    # dictionary - empty but will store all objects by <class name>.id
+
+    __objects = {}
+
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+                return new_dict
+            return self.__objects
