caf99cd9388ad4730bbf27ecf702a7598c5d407e
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-13 00:10:34 +0000

    refactoring code

--- models/base_model.py
+++ models/base_model.py
@@ -4,63 +4,30 @@
    Contains class BaseModel
 """
 
-from datetime import datetime
-import models
 import uuid
+from datetime import datetime
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
-    """The BaseModel class from  which future classes will be derived"""
+    """Parent class for all other classes to inherit from"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """Initialization of public instance attributes"""
 
-        if kwargs:
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
             for k, v in kwargs.items():
-                if k != "__class__":
+                if "__class__" not in k:
                     setattr(self, k, v)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
 
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.utcnow()
-                self.updated_at = self.created_at
-
-
+    
     def __str__(self):
-        """String representation  of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-
-
-    def save(self):
-        """Updates the attribute"""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self):
-        """returns a dictionary containing all key/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        return new_dict
-
-    def delete(self):
-        """delete the current instance from the storage"""
-        models.storage.delete(self)
+        """Returns a string representation of BaseModel class"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
