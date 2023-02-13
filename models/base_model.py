b36beba138f895879bc0fd51d2e7e37018f67848
Shakvilla <abdulshakuraclement@yahoo.com>
2023-02-13 00:16:56 +0000

    refactored basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -31,3 +31,14 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of BaseModel class"""
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance:"""
+        copy_dict = dict(self.__dict__)
+        copy_dict['__class__'] = self.__class__.__name__
+        copy_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        copy_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
